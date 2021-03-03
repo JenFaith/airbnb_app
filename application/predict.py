a943f69501057e4cb1214639d554f1b22403c698
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2021-03-03 12:54:17 -0500

    fix error with input data and prediction function

--- application/predict.py
+++ application/predict.py
@@ -7,10 +7,10 @@ from joblib import load
 # Load model
 # This should be located near the top of the app.create_app function so that
 # the model is loaded only once upon instantiation
-load_model = load('finalized_model.sav')  # TODO - Cut and paste into app.create_app
+# TODO - Cut and paste into app.create_app
 
 
-def get_prediction(user_data):
+def get_prediction(user_data, model):
     """
     Makes a prediction using data input by user on web app.
 
@@ -18,16 +18,18 @@ def get_prediction(user_data):
     ----------
     user_data : list
         Features used in Airbnb prediction model
+    model : 
+        Pretrained model used to make predictions
 
     Returns
     -------
     float
         Prediction in USD per day (e.g. 124.32)
     """
-    
+
     # Feature orders needed for model.predict's input data
     # The columns are required to be in the same order as the data input
-    feature_order = [ 
+    feature_order = [
         "property_type",
         "room_type",
         "bathrooms",
@@ -36,10 +38,10 @@ def get_prediction(user_data):
         "host_since",
         "review_scores_rating",
         "bedrooms",
-        "beds",  
+        "beds",
         "instant_bookable",
         "host_has_profile_pic",
-        "host_identity_verified",     
+        "host_identity_verified",
         "cleaning_fee",
         "Wireless Internet",
         "Air conditioning",
@@ -53,7 +55,7 @@ def get_prediction(user_data):
         "Laptop friendly workspace",
         "Indoor fireplace",
         "TV",
-        "Cable TV"
+        "Cable TV",
     ]
 
     # dictionary for forcing data types accepted by sklearn
@@ -63,11 +65,11 @@ def get_prediction(user_data):
         "bathrooms":                    np.float64,
         "cancellation_policy":          object,
         "city":                         object,
-        "host_since":                   np.int64,        
-        "review_scores_rating":         object,       
+        "host_since":                   np.int64,
+        "review_scores_rating":         object,
         "bedrooms":                     np.float64,
         "beds":                         np.float64,
-        "instant_bookable":             bool,        
+        "instant_bookable":             bool,
         "host_has_profile_pic":         bool,
         "host_identity_verified":       bool,
         "cleaning_fee":                 bool,
@@ -86,55 +88,27 @@ def get_prediction(user_data):
         "Cable TV":                     bool,
     }
 
-    # dictionary to construct dataframe
-    data = {feature: [datum]
-            for feature, datum in zip(feature_order, user_data)}
+    # Make sure the data is coming in correctly
+    # This also serves as a simple backend data validation so the route returns
+    # a value even if someone tries to spoof a data point
+    try:
+        assert len(user_data) == len(
+            feature_order), 'User_data not long enough'
 
-    # transform input into dataframe for model prediction
-    transformed_input = pd.DataFrame(data).astype(data_types)[feature_order]
+        # dictionary to construct dataframe
+        data = {feature: [datum]
+                for feature, datum in zip(feature_order, user_data)}
+        assert len(data) == len(feature_order), 'dictionary size incorrect'
 
-    # make prediction using model
-    prediction = load_model.predict(transformed_input)
+        # transform input into dataframe for model prediction
+        transformed_input = pd.DataFrame(
+            data).astype(data_types)[feature_order]
+
+        # make prediction using model
+        prediction = model.predict(transformed_input)
+    except:
+        print('-----ERROR IN TRANSFORMING INPUT-----')
+        prediction = [0]
 
     # return prediction in USD rounded to the penny
     return np.exp(prediction[0]).round(2)
-
-
-# Run tests on functions
-if __name__ == "__main__":
-
-    user_input = [
-        "Apartment",        # property_type
-        "Entire home/apt",  # room_type
-        1.0,                # bathrooms
-        "strict",           # cancellation_policy
-        True,               # cleaning_fee
-        "NYC",              # city
-        "t",                # host_has_profile_pic
-        "f",                # host_identity_verified
-        1369,               # host_since
-        "f",                # instant_bookable
-        90,                 # review_scores_rating
-        0.0,                # bedrooms
-        2.0,                # beds
-        1,                  # Wireless Internet
-        1,                  # Air conditioning
-        0,                  # Kitchen
-        1,                  # Heating
-        0,                  # Family/kid friendly
-        0,                  # Hair dryer
-        0,                  # Iron
-        0,                  # Shampoo
-        1,                  # Fire extinguisher
-        0,                  # Laptop friendly workspace
-        0,                  # Indoor fireplace
-        1,                  # TV
-        0                   # Cable TV"
-    ]
-    print('Prediction: $', get_prediction(user_input))
-
-    # TEST Some random test set values
-    X_test = pd.read_csv('data/archive-2/test_2.csv',  # TODO - Change path
-                         index_col='id').sample(10)
-    print([get_prediction(X_test.iloc[i].tolist())
-           for i in range(len(X_test))])
